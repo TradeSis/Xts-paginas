65ef8c7028829ee854f64d571072c09e891d2a74
Lucas Rosa <lucas.rosa@tradesis.com.br>
2023-07-18 12:08:09 -0300

    update repositórios

--- database/eventos.php
+++ database/eventos.php
@@ -1,6 +1,5 @@
 <?php
-include_once __DIR__."/../../config.php";
-include_once (ROOT.'/paginas/conexao.php');
+include_once ('../conexao.php');
 
 function buscaEventosSlug($slug)
 {
--- database/paginas.php
+++ database/paginas.php
@@ -1,6 +1,6 @@
 <?php
-include_once __DIR__ . "/../../config.php";
-include_once(ROOT . '/paginas/conexao.php');
+include_once ('../conexao.php');
+
 function buscaPagina($slug)
 {
 
