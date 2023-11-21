65b6d7690732371301886617da009f23c67656c1
Gabriel Bettencourt <gabriel.vieira@tradesis.com.br>
2023-11-21 10:18:07 -0300

    fix

--- secao/secao/clientes.php
+++ secao/secao/clientes.php
@@ -3,10 +3,13 @@ include_once(ROOT . '/cadastros/database/clientes.php');
 
 $clientes = buscaClientes();
 ?>
-<div class="container">
+<div class="container mt-2">
     <h2>Nossos Clientes</h2>
-    <div class="row">
+    <div class="row mt-4 mb-4">
         <?php foreach ($clientes as $cliente) { ?>
+                <div class="col-md-3">
+                    <img src="<?php echo $cliente['imgCliente'] ?>" class="ts-clienteIcon" role="img">
+                </div>
                 <div class="col-md-3">
                     <img src="<?php echo $cliente['imgCliente'] ?>" class="ts-clienteIcon" role="img">
                 </div>
--- secao/secao/filiais.php
+++ secao/secao/filiais.php
@@ -3,8 +3,8 @@ include_once(ROOT . '/cadastros/database/pessoas.php');
 
 $pessoas = buscarPessoa();
 ?>
-<div class="ts-filiais">
-    <div class="container ts-filiais ts-textoCentro">
+<div class="ts-filiais ">
+    <div class="container ts-textoCentro">
         <div class="row">
             <?php $counter = 0;
             foreach ($pessoas as $pessoa) {
@@ -12,12 +12,6 @@ $pessoas = buscarPessoa();
                     <div class="col">
                         <h4><?php echo $pessoa['municipio'] ?></h4>
                         <p><i class="bi bi-geo-alt-fill"></i> <?php echo $pessoa["endereco"] ?> | CEP: <?php echo $pessoa["cep"] ?> - <?php echo $pessoa["municipio"] ?>-<?php echo $pessoa["UF"] ?></p>
-
-                        <?php if($pessoa['endereco2'] !== null ) { ?>
-                        <hr>
-                        <p><i class="bi bi-geo-alt-fill"></i> <?php echo $pessoa["endereco2"] ?? null ?> | CEP: <?php echo $pessoa["cep"] ?? null ?> - <?php echo $pessoa["municipio"] ?? null ?>-<?php echo $pessoa["UF"] ?? null ?></p> 
-                        <?php } ?>
-
                         <p><i class="bi bi-telephone-fill"></i> <?php echo $pessoa["telefone"] ?></p> 
                     </div>
                 <?php $counter++;}
